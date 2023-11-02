22fe29a17602dc0a4e37d490eb84119cf76d66e6
hamza <seyedi.hamza@gmail.com>
2023-11-02 21:17:44 +0000

    add 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-# prints all the names defined by the compiled module hidden_4.pyc 
     import hidden_4
-
-    for index in dir(hidden_4):
-        if index[:2] != "__":
-            print(index)
+    # Print all hidden name from directory
+    for name in dir(hidden_4):
+        if name[:2] != "__":
+            print("{}".format(name))
