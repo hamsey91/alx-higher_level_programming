bfd7bfd902f0d40985abb117b3be0ed606e99efd
hamza <seyedi.hamza@gmail.com>
2023-11-08 21:06:45 +0000

    add 8

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     squared_matrix = []
     for row in matrix:
-        squared.append([e**2 for e in row])
+        squared_matrix.append([e**2 for e in row])
     return squared_matrix
