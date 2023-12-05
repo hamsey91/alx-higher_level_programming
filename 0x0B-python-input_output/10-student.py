6b4fb4e045bb565a99aa856592309ab6da79bc4d
hamza <seyedi.hamza@gmail.com>
2023-12-05 16:45:45 +0000

    add 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -21,8 +21,8 @@ class Student:
         """Returns a dictionary representation of the Student instance.
 
         If attrs is a list of strings, only attribute names contained in
-        this list must be retrieved.Otherwise, all attributes must be retrieved.
-        """
+        this list must be retrieved."""
+
         if type(attrs) == list and all(type(ele) == str for ele in attrs):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
