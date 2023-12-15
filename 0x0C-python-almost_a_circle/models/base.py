03f613282c8e3b4d387af3527ed2f71abb37a983
hamza <seyedi.hamza@gmail.com>
2023-12-15 22:45:33 +0000

    add 18

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,8 +23,49 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """Returns the JSON string representation of list_dictionaries"""
+        """Returns the JSON string representation of list_dictionaries.
+	Args:
+            list_dictionaries (list): a list of dictionaries.
+        """
         if list_dictionaries is None or not list_dictionaries:
             return "[]"
         else:
             return dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs to a file.
+	 Args:
+            list_objs (list): a list of instances who inherited of Base.
+        """
+        if list_objs is not None:
+            list_objs = [o.to_dictionary() for o in list_objs]
+        with open("{}.json".format(cls.__name__), "w", encoding="utf-8") as f:
+            f.write(cls.to_json_string(list_objs))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list of the JSON string representation json_string.
+	Args:
+            json_string (str): a string representing a list of dictionaries.
+	"""
+        if json_string is None or not json_string:
+            return []
+        return loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes already set.
+	Args:
+            **dictionary (dict): double pointer to a dictionary.
+        """
+        from models.rectangle import Rectangle
+        from models.square import Square
+        if cls is Rectangle:
+            new = Rectangle(1, 1)
+        elif cls is Square:
+            new = Square(1)
+        else:
+            new = None
+        new.update(**dictionary)
+        return new
