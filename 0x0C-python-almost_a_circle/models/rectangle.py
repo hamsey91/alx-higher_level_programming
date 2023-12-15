f4872ab8a93b54b105ab0f9727c88ae21af6a68b
hamza <seyedi.hamza@gmail.com>
2023-12-15 14:38:01 +0000

    add 9

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -84,13 +84,42 @@ class Rectangle(Base):
         return self.width * self.height
 
     def display(self):
-        '''Prints in stdout the Rectangle instance with the character #.'''
+        """Prints in stdout the Rectangle instance with the character #."""
         s = '\n' * self.y + \
             (' ' * self.x + '#' * self.width + '\n') * self.height
         print(s, end='')
 
     def __str__(self):
-        '''Returns rectangle information in string type.'''
+        """Returns rectangle information in string type."""
         return '[{}] ({}) {}/{} - {}/{}'.\
             format(type(self).__name__, self.id, self.x, self.y, self.width,
                    self.height)
+
+    def first_update(self, id=None, width=None, height=None, x=None, y=None):
+        """Method that assigns an argument to each attribute."""
+        if id is not None:
+            self.id = id
+        if width is not None:
+            self.width = width
+        if height is not None:
+            self.height = height
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        """Updates instance attributes via no-keyword & keyword args.
+	Args:
+            *args (ints): The attribute values:
+                - 1st argument represents id attribute
+                - 2nd argument represents width attribute
+                - 3rd argument represent height attribute
+                - 4th argument represents x attribute
+                - 5th argument represents y attribute
+            **kwargs (dict): The double pointer to a dictionary key/value.
+        """
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs
