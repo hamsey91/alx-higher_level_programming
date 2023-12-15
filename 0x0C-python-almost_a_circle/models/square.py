ec97b8a85fc5681dfef486172bfa565c85e4b06e
hamza <seyedi.hamza@gmail.com>
2023-12-15 15:38:15 +0000

    add 10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,11 +4,9 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Represents a square class."""
 
     def __init__(self, size, x=0, y=0, id=None):
         """Constructor.
-
         Args:
             size (int): The square size.
             x (int): the horizontal axis x of square.
@@ -16,3 +14,53 @@ class Square(Rectangle):
             id (int): the identity of the square.
         """
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Getter/setter for the square size."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def f_update(self, id=None, size=None, x=None, y=None):
+        """Method that assigns an argument to each attribute."""
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.size = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        """Updates instance attributes via no-keyword & keyword args.
+        Args:
+            *args (ints): The attribute values:
+                - 1st argument represents id attribute
+                - 2nd argument represents size attribute
+                - 3rd argument represents x attribute
+                - 4th argument represents y attribute
+            **kwargs (dict): The double pointer to a dictionary key/value.
+        """
+        if args:
+            self.f_update(*args)
+        elif kwargs:
+            self.f_update(**kwargs)
+
+    def to_dictionary(self):
+        """Method for the dictionary representation of a square."""
+        return {
+            "id": self.id,
+            "size": self.width,
+            "x": self.x,
+            "y": self.y
+        }
+
+    def __str__(self):
+        '''Returns string info about this square.'''
+        return '[{}] ({}) {}/{} - {}'.\
+            format(type(self).__name__, self.id, self.x, self.y, self.width)
