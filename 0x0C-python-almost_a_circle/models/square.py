c953276923ce5ff2a663b4fb93610dfce6cb045f
hamza <seyedi.hamza@gmail.com>
2023-12-15 16:15:21 +0000

    add 10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
-'''Module for Square class.'''
+"""Defines a square class module."""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    '''A Square class.'''
+    """Represents a square class."""
 
     def __init__(self, size, x=0, y=0, id=None):
-        '''Constructor.'''
+        """Constructor.
+        Args:
+            size (int): The square size.
+            x (int): the horizontal axis x of square.
+            y (int): the vertical axis y of square.
+            id (int): the identity of the square.
+        """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        '''Returns string info about this square.'''
+        """Returns rectangle information in string type."""
         return '[{}] ({}) {}/{} - {}'.\
             format(type(self).__name__, self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
-        '''Size of this square.'''
+        """Getter/Setter for the square size."""
         return self.width
 
     @size.setter
@@ -26,7 +32,7 @@ class Square(Rectangle):
         self.height = value
 
     def f_update(self, id=None, size=None, x=None, y=None):
-        '''Internal method that updates instance attributes via */**args.'''
+        """Method that assigns an argument to each attribute."""
         if id is not None:
             self.id = id
         if size is not None:
@@ -37,13 +43,21 @@ class Square(Rectangle):
             self.y = y
 
     def update(self, *args, **kwargs):
-        '''Updates instance attributes via no-keyword & keyword args.'''
+        """Updates instance attributes via no-keyword & keyword args.
+        Args:
+            *args (ints): The attribute values:
+                - 1st argument represents id attribute
+                - 2nd argument represents size attribute
+                - 3rd argument represents x attribute
+                - 4th argument represents y attribute
+            **kwargs (dict): The double pointer to a dictionary key/value.
+        """
         if args:
             self.f_update(*args)
         elif kwargs:
             self.f_update(**kwargs)
 
     def to_dictionary(self):
-        '''Returns dictionary representation of this class.'''
+        """Method for the dictionary representation of a square."""
         return {"id": self.id, "size": self.width,
                 "x": self.x, "y": self.y}
