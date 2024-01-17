db55c213bc8704c83358c5740d68569f6e021dac
hamza <seyedi.hamza@gmail.com>
2024-01-17 23:09:44 +0000

    sql 16

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,4 @@
+-- script that lists all cities contained in the database hbtn_0d_usa.
+-- Results must be sorted in ascending order by cities.id
+-- You can use only one SELECT statement
+SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON states.id = cities.state_id ORDER BY cities.id;
