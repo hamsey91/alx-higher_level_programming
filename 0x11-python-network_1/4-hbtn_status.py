2c0a2aae985cfd24ad7b64058fb7c51536ee62b7
hamza <seyedi.hamza@gmail.com>
2024-03-28 17:51:37 -0700

    add 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" script that fetches https://alx-intranet.hbtn.io/status"""
+import requests
+
+
+if __name__ == "__main__":
+
+    req = requests.get('https://alx-intranet.hbtn.io/status')
+    t = req.text
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
