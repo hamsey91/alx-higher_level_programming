f18218a0f90ac46be1ea1e2f0e55d1c1243a3c06
hamza <seyedi.hamza@gmail.com>
2024-04-24 10:07:56 -0700

    add

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const req = require('req');
-req.get(process.argv[2]).on('res', function (res) {
+const req = require('request');
+req.get(process.argv[2]).on('response', function (res) {
   console.log(`code: ${res.statusCode}`);
 });
