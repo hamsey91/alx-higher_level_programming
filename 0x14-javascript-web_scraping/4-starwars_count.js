241060f5605aebfb8347fd32761f2a8c2e94395d
hamza <seyedi.hamza@gmail.com>
2024-04-24 10:31:47 -0700

    add

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,12 @@
+i#!/usr/bin/node
+const req = require('request');
+req(process.argv[2], function (err, res, body) {
+  if (!err) {
+    const content = JSON.parse(body).content;
+    console.log(content.reduce((count, film) => {
+      return film.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
+  }
+});
