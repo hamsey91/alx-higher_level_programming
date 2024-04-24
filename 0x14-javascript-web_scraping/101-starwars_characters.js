c72e677a47fb6fdf08d849e718bc85275341d318
hamza <seyedi.hamza@gmail.com>
2024-04-24 11:17:01 -0700

    add

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -5,19 +5,18 @@ const Id = process.argv[2];
 const Url = `https://swapi-api.alx-tools.com/api/films/${Id}`;
 
 req(Url, function (err, res, body) {
+  if (!err) {
+    const actors = JSON.parse(body).characters;
+    printallcharacters(actors, 0);
+  }
+});
+function printallcharacters (actors, count) {
+  req(actors[count], function (err, res, body) {
     if (!err) {
-      const actors = JSON.parse(body).characters;
-      printallcharacters(actors, 0);
+      console.log(JSON.parse(body).name);
+      if (count + 1 < actors.length) {
+        printallcharacters(actors, count + 1);
+      }
     }
   });
-  
-function printallcharacters (actors, count) {
-    req(actors[count], function (err, res, body) {
-      if (!err) {
-        console.log(JSON.parse(body).name);
-        if (count + 1 < actors.length) {
-            printallcharacters(actors, count + 1);
-        }
-      }
-    });
 }
