6c946a9571e03c03dfaa6ef67729a9006cebe53b
hamza <seyedi.hamza@gmail.com>
2024-07-04 14:30:43 -0700

    first commit

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Script that sends a request to URL and displays the size of the body
+curl -s "${1}" | wc -c
---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Script tht sends a GET request, displays only the  body of a 200 status code response
+curl -Ls "$1"
