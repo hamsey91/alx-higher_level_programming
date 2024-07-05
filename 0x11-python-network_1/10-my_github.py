8702af7b8a4b8fe4dc7a4571e3c5608533317a93
hamza <seyedi.hamza@gmail.com>
2024-07-05 14:23:18 -0700

    add

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id.
+"""
+from requests import get
+from sys import argv
+
+
+if __name__ == '__main__':
+
+    username = argv[1]
+    password = argv[2]
+
+    request_url = "https://api.github.com/user"
+    response = get(request_url, auth=(username, password))
+    print(response.json().get('id'))
---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Script that sends a request to the URL,
+and displays the body of the response.
+"""
+from requests import post
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    data = {'q': argv[1] if len(argv) >= 2 else ""}
+    req = post('http://0.0.0.0:5000/search_user', data)
+
+    t_req = req.headers['content-type']
+
+    if t_req == 'application/json':
+        res = req.json()
+        s_id = res.get('id')
+        s_name = res.get('name')
+        if (res != {} and s_id and s_name):
+            print("[{}] {}".format(s_id, s_name))
+        else:
+            print('No result')
+    else:
+        print('Not a valid JSON')
